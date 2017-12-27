fa18963e95a7756cc1b14f96f1228fcb5d874362
Dmytrii Zhmurchuk <Dmytrii_Zhmurchuk@epam.com>
2017-12-27 17:43:59 +0200

    add meta tags

--- client/js/admin/APP.jsx
+++ client/js/admin/APP.jsx
@@ -5,7 +5,7 @@ const AppAdmin = () => {
   return (
   	<div className="container-fluid fullHeight">
   	 	<div className="row mainviewport fullHeight">
-			<div className="col-lg-3 col-xl-3 panel-viewport fullHeight">
+			<div className="col-sm-3 col-md-3 col-lg-3 col-xl-3 panel-viewport fullHeight">
 				<div className="panel panel-default fullHeight ">
 					<div className="panel-heading">#Menu bar</div>
     				<div className="panel-body">
@@ -13,7 +13,7 @@ const AppAdmin = () => {
 		    		</div>	
 		    	</div>		
 			</div>
-	     	<div className="col-lg-9 col-xl-9 panel-viewport fullHeight">
+	     	<div className="col-sm-9 col-md-9 col-lg-9 col-xl-9 panel-viewport fullHeight">
 	     		<div className="panel panel-default fullHeight">
 	     			<div className="panel-heading">Grid viewport</div>
     				<div className="panel-body">
